5b4b11065d050bb43043fb1bd8ec97229bb70b82
David Batey <davidlbatey@gmail.com>
2012-02-24 23:40:41 +0000

    Fixed the controller specs

--- spec/controllers/streams_controller_spec.rb
+++ spec/controllers/streams_controller_spec.rb
@@ -24,7 +24,7 @@ describe StreamsController do
   # Stream. As you add validations to Stream, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    {hashtag: "#punked"}
   end
   
   # This should return the minimal set of values that should be in the session
