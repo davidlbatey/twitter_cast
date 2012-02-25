b0bb812609c05d23d6650c2f7f875c4a24b39b6f
David Batey <davidlbatey@gmail.com>
2012-02-25 01:21:14 +0000

    Added twitter initializer

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,8 @@
+# Set up your twitter api details at dev.twitter.com
+#
+Twitter.configure do |config|
+  config.consumer_key       = "XXXXXXXXXXXXXXXXX"
+  config.consumer_secret    = "XXXXXXXXXXXXXXXXX"
+  config.oauth_token        = "XXXXXXXXXXXXXXXXX"
+  config.oauth_token_secret = "XXXXXXXXXXXXXXXXX"
+end
